7a4dab060373a4873641fa43e4145512ebfb93a9
BartoszSmok <smok.bartosz@gmail.com>
2021-01-05 09:55:14 +0100

    Added add() method

--- Gittie.cs
+++ Gittie.cs
@@ -7,11 +7,17 @@ namespace gitdojo
         public static void Main(string[] args)
         {
             Introduce();
+            Console.WriteLine(add(2,4));
         }
 
         public static void Introduce()
         {
             Console.WriteLine("Hello, I'm Gittie!");
+        }
+
+        public static int add(int a, int b)
+        {
+            return a + b;
         }
     }
 }

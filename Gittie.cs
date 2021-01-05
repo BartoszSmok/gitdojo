ae985463be6f33bbd5f2f3e785e95f59a6353a80
Krzysiek <krzysiek.at@gmail.com>
2021-01-05 09:53:50 +0100

    Added funny Joke method

--- Gittie.cs
+++ Gittie.cs
@@ -7,11 +7,17 @@ namespace gitdojo
         public static void Main(string[] args)
         {
             Introduce();
+            Joke();
         }
 
         public static void Introduce()
         {
             Console.WriteLine("Hello, I'm Gittie!");
+        }
+
+        public static void Joke()
+        {
+            Console.WriteLine("I ate a clock yesterday, it was very time-consuming.");
         }
     }
 }

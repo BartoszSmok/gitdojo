8b1eb61b2e3f4ebc713ece294d25bfa210c42e2f
Krzysiek <krzysiek.at@gmail.com>
2021-01-05 09:47:12 +0100

    Added Introduce() method

---
+++ Gittie.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace gitdojo
+{
+    public static class Gittie
+    {
+        public static void Main(string[] args)
+        {
+            Introduce();
+        }
+
+        public static void Introduce()
+        {
+            Console.WriteLine("Hello, I'm Gittie!");
+        }
+    }
+}
